1458d58cb5db61653162de992f3ab38bbee6e83d
krishipatel154 <krishipatel1542005@gmail.com>
2025-11-29 11:47:55 +0530

    database error solve

--- backend/main.py
+++ backend/main.py
@@ -33,7 +33,8 @@ app.add_middleware(
         "http://localhost:5173",  # Vite default port
         "http://127.0.0.1:3000",
         "http://127.0.0.1:5173",
-        "http://65.2.121.160:5173"
+        "http://65.2.121.160:5173",
+        "http://65.2.121.160:8000"
     ],
     allow_credentials=True,
     allow_methods=["*"],
